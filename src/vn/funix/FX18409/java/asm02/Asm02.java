0bd9ff2e825d9014a93718fbdf048ce2f2a389d0
lppduy <lephamphuongduy@gmail.com>
2024-03-16 20:25:18 +0700

    Completed the addCustomer functionality with customer existence check and success message

--- src/vn/funix/FX18409/java/asm02/Asm02.java
+++ src/vn/funix/FX18409/java/asm02/Asm02.java
@@ -2,11 +2,16 @@ package vn.funix.FX18409.java.asm02;
 
 import vn.funix.FX18409.java.asm02.controllers.ProgramController;
 import vn.funix.FX18409.java.asm02.models.Bank;
+import vn.funix.FX18409.java.asm02.models.Customer;
 
 public class Asm02 {
     public static final Bank bank = new Bank();  //  set public because other class will use this bank object
     public static void main(String[] args) {
+
+        Customer existingCustomer = new Customer("Duy", "051097222222");
+        bank.addCustomer(existingCustomer);  // for testing the existing customer case
+
         ProgramController program = new ProgramController();
-        program.run();  // CitizenId to test: 037153000257
+        program.run();  // CitizenId to test: 037153000257 (valid), 015198232323 (valid)
     }
 }
--- src/vn/funix/FX18409/java/asm02/services/CustomerService.java
+++ src/vn/funix/FX18409/java/asm02/services/CustomerService.java
@@ -1,7 +1,10 @@
 package vn.funix.FX18409.java.asm02.services;
 
+import vn.funix.FX18409.java.asm02.models.Customer;
+
 import java.util.Scanner;
 
+import static vn.funix.FX18409.java.asm02.Asm02.bank;
 public class CustomerService {
 
     private final CitizenIdServiceV2 citizenIdService = new CitizenIdServiceV2();
@@ -9,16 +12,30 @@ public class CustomerService {
     public  void addCustomer() {
         Scanner sc = new Scanner(System.in);
 
-        // Enter customer name and customerId
-
         System.out.println("Nhap ten khach hang: ");
         String customerName = sc.next();
 
-        System.out.println("Nhap so CCCD: ");
-        String customerId = sc.next();
+        String customerId;
+        do {
+            System.out.println("Nhap so CCCD: ");
+            customerId = sc.next();
+            if (bank.isCustomerExisted(customerId)) {
+                System.out.println("Khach hang voi CCCD nay da ton tai. Vui long nhap lai.");
+                continue;
+            }
+            if (!citizenIdService.validateCitizenId(customerId)) {
+                System.out.println("So CCCD khong hop le.");
+                System.out.println("Vui long nhap lai hoac nhap 'No' de thoat:");
+                if ("No".equalsIgnoreCase(customerId)) return;
+            } else {
+                break;
+            }
+        } while (true);
 
-        boolean isValid = citizenIdService.validateCitizenId(customerId);
-        System.out.println(isValid);
-
+        // add new customer to bank
+        Customer newCustomer = new Customer(customerName, customerId);
+        bank.addCustomer(newCustomer);
+        System.out.println("Da them khach hang " + customerId + " vao danh sach");
     }
+
 }

b3b8dcc7bfa14cf742c584008f53be139d92da1c
lppduy <lephamphuongduy@gmail.com>
2024-03-17 14:32:48 +0700

    Completed the addAccountForCustomer feature

--- src/vn/funix/FX18409/java/asm02/services/AccountService.java
+++ src/vn/funix/FX18409/java/asm02/services/AccountService.java
@@ -1,4 +1,80 @@
 package vn.funix.FX18409.java.asm02.services;
 
+import vn.funix.FX18409.java.asm02.models.Account;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import static vn.funix.FX18409.java.asm02.Asm02.bank;
 public class AccountService {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public static Account createAccountForCustomer(){
+        String accountNumber = getAccountNumberFromUser();
+        double accountBalance = getAccountBalanceFromUser();
+        double validatedAccountBalance = validateAccountBalance(accountBalance);
+
+        return new Account(accountNumber, validatedAccountBalance);
+    }
+
+    private static String getAccountNumberFromUser() {
+        System.out.println("Nhap ma STK gom 6 chu so");
+        String accountNumber = sc.next();
+        while(!checkAccountNumber(accountNumber)) {
+            System.out.println("STK chua chinh xac hoac da ton tai vui long nhap lai: ");
+            accountNumber = sc.next();
+        }
+        return accountNumber;
+    }
+
+    private static double getAccountBalanceFromUser() {
+        System.out.println("Nhap so du: ");
+        double accountBalance = 0;
+        boolean validInput = false;
+        while (!validInput) {
+            try {
+                accountBalance = sc.nextDouble();
+                validInput = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Nhap khong hop le. Vui long nhap mot so.");
+                sc.next(); // clear the invalid input
+            }
+        }
+        return accountBalance;
+    }
+
+    private static double validateAccountBalance(double accountBalance) {
+        while(accountBalance < 50000) {
+            System.out.println("So du tai khoan khong duoc thap hon 50,000đ, vui long nhap lai");
+            accountBalance = sc.nextDouble();
+        }
+        return accountBalance;
+    }
+
+    private static boolean checkAccountNumber(String accountNumber) {
+        boolean checkAccountNumber = true;
+        // Id.length = 12 character.
+        if(accountNumber.length() != 6) checkAccountNumber = false;
+
+        // Id is Number or notNumber?
+        for (int i = 0; i < accountNumber.length(); i++) {
+            String s = "";
+            s += accountNumber.charAt(i);
+            int intValue;
+            try {
+                intValue = Integer.parseInt(s);
+            } catch (NumberFormatException e) {
+                checkAccountNumber = false;
+                break;
+            }
+        }
+
+        for (int i = 0; i < bank.getCustomers().size(); i++) {
+            for (int j = 0; j < bank.getCustomers().get(i).getAccounts().size(); j++) {
+                if(accountNumber.equals(bank.getCustomers().get(i).getAccounts().get(j).getAccountNumber())) checkAccountNumber = false;
+            }
+        }
+        return checkAccountNumber;
+    }
 }
--- src/vn/funix/FX18409/java/asm02/services/BankService.java
+++ src/vn/funix/FX18409/java/asm02/services/BankService.java
@@ -1,10 +1,21 @@
 package vn.funix.FX18409.java.asm02.services;
 
 import vn.funix.FX18409.java.asm02.models.Account;
+import vn.funix.FX18409.java.asm02.utils.CurrencyFormatter;
+
+import java.util.Scanner;
+
+import static vn.funix.FX18409.java.asm02.Asm02.bank;
 
 public class BankService {
 
+    private final CitizenIdService citizenIdService = new CitizenIdService();
+
     public void addAccountForCustomer() {
-        System.out.println("addAccountForCustomer");
+        String customerId = citizenIdService.getAndValidateCitizenIdFromUser();
+        Account  newAccount = AccountService.createAccountForCustomer();
+        bank.addAccount(customerId, newAccount);
+        System.out.println("Them thanh cong STK " + newAccount.getAccountNumber() + " voi so du : " + CurrencyFormatter.format(newAccount.getBalance()) + " vao khach hang co ma CCCD : " + customerId );
     }
+
 }

9af8d55001e6fa281412acd35d0a9d87d174819d
lppduy <lephamphuongduy@gmail.com>
2024-03-17 13:57:36 +0700

    Added getAndValidateCitizenIdFromUser method to CitizenIdService and utilized it in CustomerService

--- src/vn/funix/FX18409/java/asm02/services/CustomerService.java
+++ src/vn/funix/FX18409/java/asm02/services/CustomerService.java
@@ -7,30 +7,16 @@ import java.util.Scanner;
 import static vn.funix.FX18409.java.asm02.Asm02.bank;
 public class CustomerService {
 
-    private final CitizenIdServiceV2 citizenIdService = new CitizenIdServiceV2();
+    private final CitizenIdService citizenIdService = new CitizenIdService();
 
-    public  void addCustomer() {
+    public void addCustomer() {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Nhap ten khach hang: ");
         String customerName = sc.next();
 
-        String customerId;
-        do {
-            System.out.println("Nhap so CCCD: ");
-            customerId = sc.next();
-            if (bank.isCustomerExisted(customerId)) {
-                System.out.println("Khach hang voi CCCD nay da ton tai. Vui long nhap lai.");
-                continue;
-            }
-            if (!citizenIdService.validateCitizenId(customerId)) {
-                System.out.println("So CCCD khong hop le.");
-                System.out.println("Vui long nhap lai hoac nhap 'No' de thoat:");
-                if ("No".equalsIgnoreCase(customerId)) return;
-            } else {
-                break;
-            }
-        } while (true);
+        String customerId = citizenIdService.getAndValidateCitizenIdFromUser();
+        if (customerId == null) return;
 
         // add new customer to bank
         Customer newCustomer = new Customer(customerName, customerId);
@@ -49,5 +35,7 @@ public class CustomerService {
     public void findCustomerByName() {
         System.out.println("findCustomerByName");
     }
+
+
 
 }
